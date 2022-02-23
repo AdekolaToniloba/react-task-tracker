2a932438581051efa0ed2f482ec27ba2b62fbe99
AdekolaToniloba <atoniloba@gmail.com>
2022-02-23 15:33:03 +0100

    adding delete functionality

--- src/App.js
+++ src/App.js
@@ -27,10 +27,20 @@ function App() {
     },
   ]);
 
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) =>{
+    
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
     </div>
   );
 }
